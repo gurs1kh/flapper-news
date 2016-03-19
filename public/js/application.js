96daa9cc8b5c8c08bb84e1708c89f729659f5e9a
gurs1kh <gurs1kh@live.com>
2016-03-18 18:00:29 -0700

    some refactoring

--- public/js/application.js
+++ public/js/application.js
@@ -15,7 +15,7 @@ app.config(['$stateProvider', '$urlRouterProvider',
 			})
 			.state('posts', {
 				url: '/posts/{id}',
-				templateUrl: '/views/post.html',
+				templateUrl: '/views/posts.html',
 				controller: 'PostsCtrl',
 				resolve: {
 					post: ['$stateParams', 'posts', function($stateParams,posts) {
@@ -47,6 +47,7 @@ app.config(['$stateProvider', '$urlRouterProvider',
 	}
 ])
 
+	// Main controller
 app.controller('MainCtrl', ['$scope', 'posts', 'auth',
 	function($scope, posts, auth){
 		$scope.isLoggedIn = auth.isLoggedIn;
@@ -69,8 +70,9 @@ app.controller('MainCtrl', ['$scope', 'posts', 'auth',
 			posts.upvote(post);
 		};
 	}
-]);
+])
 
+	// Post controller
 app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth',
 	function($scope, posts, post, auth) {
 		$scope.isLoggedIn = auth.isLoggedIn;
@@ -94,46 +96,48 @@ app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth',
 	}
 ])
 
+// Angular service
 app.factory('posts', ['$http', 'auth',
 	function($http, auth){
+		// service body
 		var o = {
 			posts: []
 		};
-
+		// get all posts
 		o.getAll = function() {
 			return $http.get('/posts').success(function(data) {
 				angular.copy(data, o.posts);
 			});
 		};
-
+		// create new posts
 		o.create = function(post) {
 			return $http.post('/posts', post).success(function(data) {
 				o.posts.push(data);
 			});
 		};
-
+		// upvote
 		o.upvote = function(post) {
 			return $http.put('/posts/' + post._id + '/upvote').success(function(data) {
 				post.upvotes += 1;
 			});
 		};
-
+		// get single post
 		o.get = function(id) {
 			return $http.get('/posts/' + id).then(function(res) {
 				return res.data;
 			});
 		};
-
+		// delete single post
 		o.delete = function(post) {
 			return $http.delete('/posts/' + post._id).success(function(data) {
 				angular.copy(data, o.posts);
 			});
 		}
-
+		// add comment
 		o.addComment = function(id, comment) {
 			return $http.post('/posts/' + id + '/comments', comment);
 		};
-
+		// upvote comment
 		o.upvoteComment = function(post, comment) {
 			return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
 				.success(function(data) {
